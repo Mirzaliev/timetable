4d0af553d3d45196479112e66b0ba620f5e534f6
Mirzaliev <arsen.mirzaliev@mail.ru>
2019-09-17 17:50:14 +0300

    Make search input and search functional

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,50 @@
 <template>
   <section class="index-section searchPage">
-    <div class="searchPage__title">ПОИСК</div>
-    <v-select
-      label="name"
-      :filterable="false"
-      :options="options"
-      @search="onSearch"
-    >
-      <template slot="no-options">
-        По запросу ничего не найдено...
-      </template>
-      <template slot="option" slot-scope="option">
-        <div class="d-center">
-          {{ option.full_name }}
-        </div>
-      </template>
-      <template slot="selected-option" slot-scope="option">
-        <div class="selected d-center">
-          {{ option.full_name }}
-        </div>
-      </template>
-    </v-select>
+    <div class="searchPage-container">
+      <div class="searchPage-search-wrap">
+        <div class="searchPage__title">ПОИСК</div>
+        <form action="GET" class="searchPage-form">
+          <v-select
+            label="name"
+            :filterable="false"
+            :options="options"
+            @input="changeRoute"
+            @search="onSearch"
+          >
+            <template slot="no-options">
+              По запросу ничего не найдено...
+            </template>
+            <template slot="option" slot-scope="option">
+              <div class="d-center">
+                {{ option.full_name }}
+              </div>
+            </template>
+            <template slot="open-indicator">
+              <svg
+                version="1.1"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+                class="vs__actions_icon"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                enable-background="new 0 0 512 512"
+              >
+                <g>
+                  <path
+                    d="M495,466.2L377.2,348.4c29.2-35.6,46.8-81.2,46.8-130.9C424,103.5,331.5,11,217.5,11C103.4,11,11,103.5,11,217.5   S103.4,424,217.5,424c49.7,0,95.2-17.5,130.8-46.7L466.1,495c8,8,20.9,8,28.9,0C503,487.1,503,474.1,495,466.2z M217.5,382.9   C126.2,382.9,52,308.7,52,217.5S126.2,52,217.5,52C308.7,52,383,126.3,383,217.5S308.7,382.9,217.5,382.9z"
+                  />
+                </g>
+              </svg>
+            </template>
+          </v-select>
+          <span class="searchPage-form__hint"
+            >Например: ПИ, Иванова, Андр, ауд, 382, 811..</span
+          >
+        </form>
+      </div>
+      <div class="searchPage-tabs-wrap">
+        <pre> {{ json.owner }}</pre>
+      </div>
+    </div>
   </section>
 </template>
 
@@ -33,10 +57,14 @@ export default {
   data() {
     return {
       visible: false,
-      options: []
+      options: [],
+      json: {}
     }
   },
   methods: {
+    changeRoute(person) {
+      this.json = person
+    },
     onSearch(search, loading) {
       loading(true)
       this.search(loading, search, this)
@@ -55,20 +83,104 @@ export default {
 </script>
 
 <style lang="sass">
+@import "../assets/sass/vars"
 @import "~vue-select/src/scss/vue-select.scss"
 .index-section
-  display: flex
-  margin: 12rem auto 0 auto
-  justify-content: center
-  flex-direction: column
-  width: 582px
-  .v-select
+  position: relative
+  width: 100%
+.searchPage-form
+  &__hint
+    font-size: .9em
+    color: $grey-for-text
+    line-height: 1.1
+    text-overflow: ellipsis
+    white-space: nowrap
+    padding-left: 1.2rem
+.searchPage-tabs-wrap
+  margin: 3rem 0
+.searchPage-container
+  width: 600px
+  margin: 0 auto
+  padding: 0 1rem
+.searchPage-search-wrap
+  margin-top: 12rem
+.searchPage
+  &__title
     width: 100%
-.index-section
+    font-family: $medium
+    font-size: 5rem
+    line-height: 1.1
+    text-align: center
+    word-wrap: break-word
+    white-space: nowrap
+    margin-bottom: 3rem
+.searchPage
+  .v-select
+    position: relative
+    width: 100%
+    .vs__dropdown-toggle
+      display: flex
+      border-radius: 24px
+      box-shadow: none
+      z-index: 3
+      height: 47px
+      margin: 0 auto
+      padding: 0 1.2rem 0 1.2rem
+    .vs__selected-options
+      // padding-left: 1.2rem
+    .vs__search
+      background: white
+      padding: 0
+      margin: 0
+      word-wrap: break-word
+      font-size: 1.2em
+      font-family: inherit
+    .vs__selected
+      padding: 0
+      margin: 0
+      font-family: inherit
+    .vs__actions
+      padding: 0
+      margin: 0
+      .vs__clear
+        visibility: hidden
+        opacity: 0
+        display: none!important
+      &_icon
+        width: 1.1rem
+        fill: $grey-for-text
+      .vs__spinner
+        font-size: 4px
+        border-top: 0.5em solid rgba(100, 100, 100, 0.1)
+        border-right: 0.5em solid rgba(100, 100, 100, 0.1)
+        border-bottom: 0.5em solid rgba(100, 100, 100, 0.1)
+        border-left: 0.5em solid $accent
+  .vs__dropdown-toggle:hover
+    box-shadow: 0 1px 6px 0 rgba(32,33,36,0.28)
+    border-color: rgba(223,225,229,0)
+  .vs--loading
+    .vs__actions
+      svg.vs__actions_icon
+        display: none
+.searchPage .vs--open
+  .vs__dropdown-menu
+    overflow-y: hidden
+    border: 0
+    border-bottom-left-radius: 24px
+    border-bottom-right-radius: 24px
+    box-shadow: 0 4px 6px 0 rgba(32,33,36,0.28)
   .vs__dropdown-toggle
-    background-color: #41b883
-    border-radius: 30px
-  .vs__search
-    background-color: #35495e
-    color: white
+    border-bottom-left-radius: 0
+    border-bottom-right-radius: 0
+    border-bottom: 1px solid rgba(60, 60, 60, 0.26)
+@include respond-below(phone)
+  .searchPage-search-wrap
+    margin-top: 5rem
+  .searchPage__title
+    font-size: 4rem
+  .searchPage-container
+    width: 405px
+    padding: 0 .5rem
+  .searchPage-tabs-wrap
+    overflow: auto
 </style>
